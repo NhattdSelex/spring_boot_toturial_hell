66001957c3b4e2f63ccc97b59c0173866df8ba22
jt trinh <Nhattd@selex.vn>
2023-12-20 03:44:39 +0700

    init project

---
+++ src/main/java/com/example/jt_trinh_api_web/JtTrinhApiWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.jt_trinh_api_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtTrinhApiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtTrinhApiWebApplication.class, args);
+	}
+
+}
